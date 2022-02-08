01d0625702e5aabf1efd9cdecccf852f6f472c7e
gaozd <gaozd@glondon.com>
2022-02-09 07:10:53 +0800

    000001 实现Hello World！

--- src/MiniAPI/Program.cs
+++ src/MiniAPI/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var app = WebApplication.Create();
+app.MapGet("/", () => "Hello World!");
+app.Run();
