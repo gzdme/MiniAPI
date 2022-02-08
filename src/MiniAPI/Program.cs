8dfe1521f83e46e162f42adb1b6531f406383c90
gaozd <gaozd@glondon.com>
2022-02-09 07:40:49 +0800

    000003 添加试题查询接口

--- src/MiniAPI/Program.cs
+++ src/MiniAPI/Program.cs
@@ -1,4 +1,5 @@
 ﻿var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/question/{id:int}", (int id) => $"查询Id为{id}试题");
 app.Run();
