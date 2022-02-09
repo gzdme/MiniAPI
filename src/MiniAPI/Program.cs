e7ba49378c4a33df00c56cf77ed1e9105e1eae2f
gaozd <gaozd@glondon.com>
2022-02-09 08:19:20 +0800

    000004 添加路由类型Uint

--- src/MiniAPI/Program.cs
+++ src/MiniAPI/Program.cs
@@ -1,5 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder();
+﻿using MiniAPI.MyRouteConstraint;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRouting(options =>
+{
+    options.ConstraintMap["Uint"] = typeof(MiniAPI.MyRouteConstraint.Uint);
+});
+
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/question/{id:int}", (int id) => $"查询Id为{id}试题");
+//app.MapGet("/question/{id:int}", (int id) => $"查询Id为{id}试题");
+app.MapGet("/question/{id:Uint}", (uint id) => $"查询Id为{id}试题");
 app.Run();
