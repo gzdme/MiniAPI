d172a5ac8b406cabfd448dd61cd284564c2cdb59
gaozd <gaozd@glondon.com>
2022-02-09 09:44:36 +0800

    000005 路由支持正则表达式

--- src/MiniAPI/Program.cs
+++ src/MiniAPI/Program.cs
@@ -9,5 +9,8 @@ builder.Services.AddRouting(options =>
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 //app.MapGet("/question/{id:int}", (int id) => $"查询Id为{id}试题");
+// 自定义类型Uint
 app.MapGet("/question/{id:Uint}", (uint id) => $"查询Id为{id}试题");
+// 支持正则表达式，如邮编号码
+app.MapGet("/area/{postcode:regex(^[0-9]{{3}}-[0-9]{{4}}$)}", (string postcode) => $"邮编：{postcode}");
 app.Run();
